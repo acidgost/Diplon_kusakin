7af09e9505d5fc1a5532fecefcc1eb7733998abd
whereareac <ac1dghost@yandex.ru>
2025-04-30 21:05:13 +0500

    Создаан класс Users

---
+++ Diplon_kusakin/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplon_kusakin
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string Full_Name { get; set; }
+        public string Position { get; set; }
+        public string Contact_Information { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
