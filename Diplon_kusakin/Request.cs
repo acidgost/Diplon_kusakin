05af971f817083d3dcb8ab6991b4e71b17b39c70
whereareac <ac1dghost@yandex.ru>
2025-04-30 21:08:28 +0500

    Добавлена Страница Main

---
+++ Diplon_kusakin/Request.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplon_kusakin
+{
+    internal class Request
+    {
+        public int Id { get; set; }
+        public string Registration_Date { get; set; }
+        public string Equipment_Type { get; set; }
+        public string Equipment { get; set; }
+        public string ProblemDescription { get; set; }
+        public string Contact_Information { get; set; }
+        public string Status { get; set; }
+        public string Assignee { get; set; }
+        public string Priority { get; set; }
+        public string Full_name { get; set; }
+        public string DateEnd { get; set; }
+        public string Prosmotr { get; set; }
+        public string Kabinet { get; set; }
+    }
+}
